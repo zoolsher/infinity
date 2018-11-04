3525efe19f65710bdfaaaad06a284f6988af05c5
zoolsher <zoolsher@gmail.com>
2018-11-04 21:32:41 +0800

    WIP

--- infinity/parser/src/parser/xml.ts
+++ infinity/parser/src/parser/xml.ts
@@ -1,3 +1,36 @@
-import { parse } from 'fast-xml-parser';
+import * as FXP from 'fast-xml-parser';
 
-export { parse };
+export function getTemplate(x: string) {
+  // TODO: update a normal xml parser
+  const result = {
+    script: '',
+    scriptLang: '',
+    style: '',
+    styleLang: '',
+    template: '',
+  };
+  const templateStart = x.indexOf('<template>') + '<template>'.length;
+  const templateEnd = x.lastIndexOf('</template>');
+  result.template = x.substring(templateStart, templateEnd);
+  const scriptStart = x.indexOf('<script');
+  const scriptEnd = x.lastIndexOf('</script>') + '</script>'.length;
+  result.script = x.substring(scriptStart, scriptEnd);
+  const scriptResult = FXP.parse(result.script, {
+    attrNodeName: 'attr',
+    attributeNamePrefix: '',
+    ignoreAttributes: false,
+  });
+  result.scriptLang =
+    scriptResult && scriptResult.script && scriptResult.script.attr && scriptResult.script.attr.lang;
+  const styleStart = x.indexOf('<style');
+  const styleEnd = x.lastIndexOf('</style>') + '</style>'.length;
+  result.style = x.substring(styleStart, styleEnd);
+  const styleResult = FXP.parse(result.style, {
+    attrNodeName: 'attr',
+    attributeNamePrefix: '',
+    ignoreAttributes: false,
+  });
+  result.styleLang =
+    styleResult && styleResult.style && styleResult.style.attr && styleResult.style.attr.lang;
+  return result;
+}
